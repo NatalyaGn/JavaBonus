469bdcca1a6b3d4d900c8b9b8a19001480022593
NatalyaGn <107914616+NatalyaGn@users.noreply.github.com>
2022-08-24 11:55:07 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,14 +1,14 @@
 public class main {
     public static void main(String[] args) {
         int balance = 800;// начальный счёт
-        int pay = 2500;// сумма пополниения
+        int pay = 2500;// сумма пополнения
         int bonys;
         if (pay > 1000) {
             bonys = pay / 1000;
         } else {
                 bonys = 0;
             }
-            int finalBalans = balance + pay + bonys;
+            int finalBalans = balance + pay + bonys; //итоговый счёт
             System.out.println("итоговый счёт " + finalBalans);
             System.out.println("сумма бонусов " + bonys);
         }
